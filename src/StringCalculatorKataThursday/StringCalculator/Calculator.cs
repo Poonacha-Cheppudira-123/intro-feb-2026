893a1925e7be5079ae4074f02409927220a1e4d3
Poonacha-Cheppudira-123 <poonachausa@gmail.com>
2026-02-12 21:58:36 +0000

    Pulled in latest notes, code, + added my work for testing exercises - 2/12/2026

---
+++ src/StringCalculatorKataThursday/StringCalculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+       
+        List<string> values = numbers.Split(',', '\n').ToList();
+        int sum = 0;
+        foreach (string value in values)
+        {
+            sum += int.Parse(value);
+        }
+
+        return sum;
+    }
+}
